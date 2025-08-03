ca061fcf64b211194ce6746950908cf14319570a
vdatanet <joan@vdata.net>
2025-08-03 22:10:27 +0200

    Actualizar configuración de Swagger para ejecutarse fuera del entorno de desarrollo.

--- erp.Blazor.Server/Startup.cs
+++ erp.Blazor.Server/Startup.cs
@@ -219,14 +219,20 @@ namespace erp.Blazor.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "erp WebApi v1");
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "erp WebApi v1");
-                });
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c =>
+                //{
+                    //c.SwaggerEndpoint("/swagger/v1/swagger.json", "erp WebApi v1");
+                //});
             }
             else
             {
