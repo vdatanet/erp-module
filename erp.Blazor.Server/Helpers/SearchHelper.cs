1484c0bf7cfa502e09a2e8cd55edc07e2b261be7
vdatanet <joan@vdata.net>
2025-08-05 15:39:44 +0200

    Refactorizar `SearchHelper` para simplificar espacios de nombres y formato del código.

--- erp.Blazor.Server/Helpers/SearchHelper.cs
+++ erp.Blazor.Server/Helpers/SearchHelper.cs
@@ -1,65 +1,58 @@
 using System.Linq.Expressions;
 
-namespace erp.Blazor.Server.Helpers
+namespace erp.Blazor.Server.Helpers;
+
+public static class SearchHelper
 {
-    public static class SearchHelper
+    public static IQueryable<T> ApplySearchFilter<T>(
+        this IQueryable<T> query,
+        string? search,
+        params Expression<Func<T, string>>[] propertySelectors)
     {
-        public static IQueryable<T> ApplySearchFilter<T>(
-            this IQueryable<T> query, 
-            string? search,
-            params Expression<Func<T, string>>[] propertySelectors)
+        if (string.IsNullOrWhiteSpace(search) || !propertySelectors.Any()) return query;
+
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var containsMethod = typeof(string).GetMethod("Contains", [typeof(string)]);
+        var searchConstant = Expression.Constant(search);
+
+        Expression? combinedExpression = null;
+        foreach (var selector in propertySelectors)
         {
-            if (string.IsNullOrWhiteSpace(search) || !propertySelectors.Any())
-            {
-                return query;
-            }
+            var memberAccess = Expression.Invoke(selector, parameter);
+            var condition = Expression.Call(memberAccess, containsMethod!, searchConstant);
 
-            var parameter = Expression.Parameter(typeof(T), "x");
-            var containsMethod = typeof(string).GetMethod("Contains", [typeof(string)]);
-            var searchConstant = Expression.Constant(search);
-
-            Expression? combinedExpression = null;
-            foreach (var selector in propertySelectors)
-            {
-                var memberAccess = Expression.Invoke(selector, parameter);
-                var condition = Expression.Call(memberAccess, containsMethod!, searchConstant);
-                
-                combinedExpression = combinedExpression == null 
-                    ? condition 
-                    : Expression.OrElse(combinedExpression, condition);
-            }
-
-            var lambda = Expression.Lambda<Func<T, bool>>(combinedExpression!, parameter);
-            return query.Where(lambda);
+            combinedExpression = combinedExpression == null
+                ? condition
+                : Expression.OrElse(combinedExpression, condition);
         }
 
-        public static IQueryable<T> ApplySearchFilter<T>(
-            this IQueryable<T> query,
-            string? search,
-            params string[] propertyNames)
+        var lambda = Expression.Lambda<Func<T, bool>>(combinedExpression!, parameter);
+        return query.Where(lambda);
+    }
+
+    public static IQueryable<T> ApplySearchFilter<T>(
+        this IQueryable<T> query,
+        string? search,
+        params string[] propertyNames)
+    {
+        if (string.IsNullOrWhiteSpace(search) || !propertyNames.Any()) return query;
+
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var containsMethod = typeof(string).GetMethod("Contains", [typeof(string)]);
+        var searchConstant = Expression.Constant(search);
+
+        Expression? combinedExpression = null;
+        foreach (var propertyName in propertyNames)
         {
-            if (string.IsNullOrWhiteSpace(search) || !propertyNames.Any())
-            {
-                return query;
-            }
+            var property = Expression.Property(parameter, propertyName);
+            var condition = Expression.Call(property, containsMethod!, searchConstant);
 
-            var parameter = Expression.Parameter(typeof(T), "x");
-            var containsMethod = typeof(string).GetMethod("Contains", [typeof(string)]);
-            var searchConstant = Expression.Constant(search);
-
-            Expression? combinedExpression = null;
-            foreach (var propertyName in propertyNames)
-            {
-                var property = Expression.Property(parameter, propertyName);
-                var condition = Expression.Call(property, containsMethod!, searchConstant);
-                
-                combinedExpression = combinedExpression == null 
-                    ? condition 
-                    : Expression.OrElse(combinedExpression, condition);
-            }
-
-            var lambda = Expression.Lambda<Func<T, bool>>(combinedExpression!, parameter);
-            return query.Where(lambda);
+            combinedExpression = combinedExpression == null
+                ? condition
+                : Expression.OrElse(combinedExpression, condition);
         }
+
+        var lambda = Expression.Lambda<Func<T, bool>>(combinedExpression!, parameter);
+        return query.Where(lambda);
     }
 }
