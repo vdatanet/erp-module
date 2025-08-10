5292535ba9b485bc7c977bd3c4c23f130c2c5c1f
vdatanet <joan@vdata.net>
2025-08-10 12:25:38 +0200

    Añadir propiedades `DefaultSalesAccount` y `DefaultPurchaseAccount` a `CompanyInfo` con acceso mediante métodos getter/setter. Actualizar ícono asociado a la clase.

--- erp.Module/BusinessObjects/Contacts/CompanyInfo.cs
+++ erp.Module/BusinessObjects/Contacts/CompanyInfo.cs
@@ -1,16 +1,32 @@
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
+using erp.Module.BusinessObjects.Accounting;
 
 namespace erp.Module.BusinessObjects.Contacts;
 
 [DefaultClassOptions]
 [NavigationItem("Contacts")]
-[ImageName("Actions_Info")]
+[ImageName("BO_MyDetails")]
 [RuleObjectExists("CompanyInfoExists", DefaultContexts.Save, "True", InvertResult = true,
     CustomMessageTemplate = "Company Info already exists.")]
 [RuleCriteria("NotDeleteCompanyInfo", DefaultContexts.Delete, "False",
     CustomMessageTemplate = "Can't delete Company Info.")]
 public class CompanyInfo(Session session) : Contact(session)
 {
+    private Account _defaultSalesAccount;
+    private Account _defaultPurchaseAccount;
+    
+    public Account DefaultSalesAccount
+    {
+        get => _defaultSalesAccount;
+        set => SetPropertyValue(nameof(DefaultSalesAccount), ref _defaultSalesAccount, value);
+    }
+    
+    public Account DefaultPurchaseAccount
+    {
+        get => _defaultPurchaseAccount;
+        set => SetPropertyValue(nameof(DefaultPurchaseAccount), ref _defaultPurchaseAccount, value);
+    }
+    
 }
