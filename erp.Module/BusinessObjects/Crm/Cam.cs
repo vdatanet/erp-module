39613f9b2c0f915c4e85d9494b640fee470bf3d8
vdatanet <joan@vdata.net>
2025-08-14 21:16:41 +0200

    Añadir clase `Cam` con propiedades `Name`, `Description` y `CreatedDate`

---
+++ erp.Module/BusinessObjects/Crm/Cam.cs
@@ -0,0 +1,29 @@
+using DevExpress.Xpo;
+using erp.Module.BusinessObjects.Base.Common;
+
+namespace erp.Module.BusinessObjects.Crm;
+
+public class Cam(Session session): BaseEntity(session)
+{
+    private string _name;
+    private string _description;
+    private DateTime _createdDate;  
+    private string _notes;
+    private bool _isActive;
+    private bool _isAvailableInSales;
+    private bool _isAvailableInPurchases;
+
+    public string Name
+    {
+        get => _name;
+        set => SetPropertyValue(nameof(Name), ref _name, value);
+    }
+    
+    public string Description
+    {
+        get => _description;
+        set => SetPropertyValue(nameof(Description), ref _description, value);
+    }
+    
+    
+}
