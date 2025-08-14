1244fb7069bd74ac0664c423d5f21c8bedd81d0d
vdatanet <joan@vdata.net>
2025-08-14 21:57:09 +0200

    Añadir clases `Lead` y `Test` en el módulo CRM

---
+++ erp.Module/BusinessObjects/Crm/Test.cs
@@ -0,0 +1,10 @@
+using System;
+using DevExpress.Xpo;
+using erp.Module.BusinessObjects.Base.Common;
+
+namespace erp.Module.BusinessObjects.Crm;
+
+public class Test(Session session) : BaseEntity(session )
+{
+
+}
