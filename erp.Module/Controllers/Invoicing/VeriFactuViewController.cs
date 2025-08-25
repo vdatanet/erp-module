385c0a7f69e998f086353fc02f7193e15e25c792
vdatanet <joan@vdata.net>
2025-08-25 10:52:23 +0200

    Añadir VeriFactuViewController para gestión de vistas
    Se ha creado la clase `VeriFactuViewController` que hereda de `ViewController`, incluyendo métodos para manejar su ciclo de vida como `OnActivated`, `OnViewControlsCreated` y `OnDeactivated`. También se ha implementado el método `Dispose` en el archivo `VeriFactuViewController.Designer.cs` para la limpieza de recursos. Se han agregado directivas `using` para incluir las bibliotecas necesarias de DevExpress y otros espacios de nombres, facilitando así la personalización y el manejo de vistas en la aplicación.

---
+++ erp.Module/Controllers/Invoicing/VeriFactuViewController.cs
@@ -0,0 +1,45 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace erp.Module.Controllers.Invoicing
+{
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ViewController.
+    public partial class VeriFactuViewController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public VeriFactuViewController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
