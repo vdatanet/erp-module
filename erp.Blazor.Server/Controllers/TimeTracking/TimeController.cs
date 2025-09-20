fc74287b4a4673ab80582a7a2c2ef6822503c002
vdatanet <joan@vdata.dev>
2025-09-20 20:18:04 +0200

    Renombrar TimesheetEntries a TimeController y simplificar su lógica eliminando métodos innecesarios.

---
+++ erp.Blazor.Server/Controllers/TimeTracking/TimeController.cs
@@ -0,0 +1,14 @@
+#nullable enable
+using erp.Application.Interfaces.TimeTracking;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace erp.Blazor.Server.Controllers.TimeTracking;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/time")]
+public class TimeController(ITimesheetEntry service) : ControllerBase
+{
+
+}
