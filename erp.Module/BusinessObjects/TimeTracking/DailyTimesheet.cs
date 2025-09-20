1045ce6286fcdd80639d6c3bb3cb513f54de216c
vdatanet <joan@vdata.net>
2025-09-20 13:05:03 +0200

    Actualizar `DailyTimesheet` con nueva propiedad `Notes` y lógica mejorada:
    - Agregar propiedad `Notes` con soporte para varias líneas.
    - Ajustar lógica de cálculo y guardar para incluir `Recalculate` basado en `WorkdayRule`.
    - Simplificar y limpiar importaciones de namespaces.

--- erp.Module/BusinessObjects/TimeTracking/DailyTimesheet.cs
+++ erp.Module/BusinessObjects/TimeTracking/DailyTimesheet.cs
@@ -5,14 +5,10 @@ using DevExpress.ExpressApp.Xpo;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.Validation;
 using DevExpress.Xpo;
-using DevExpress.XtraSpreadsheet.Model.CopyOperation;
 using erp.Module.BusinessObjects.Base.Common;
 using erp.Module.BusinessObjects.Helpers;
 using erp.Module.BusinessObjects.Invoicing;
-using erp.Module.BusinessObjects.Projects;
 using Microsoft.Extensions.DependencyInjection;
-using VeriFactu.Xml.Factu;
-using VeriFactu.Xml.Factu.Alta;
 using SequenceFactory = erp.Module.Factories.SequenceFactory;
 
 namespace erp.Module.BusinessObjects.TimeTracking;
@@ -30,6 +26,7 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
     private TimeSpan _totalWork;
     private bool _isLateIn;
     private bool _isEarlyOut;
+    private string _notes;
 
     [Association("ApplicationUser-DailyTimesheets")]
     [ModelDefault("AllowEdit", "False")]
@@ -39,7 +36,7 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
         get => _employee;
         set => SetPropertyValue(nameof(Employee), ref _employee, value);
     }
-    
+
     [RuleRequiredField]
     public string DailyTimesheetPrefix
     {
@@ -52,7 +49,7 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
         get => _dailyTimesheetSequence;
         set => SetPropertyValue(nameof(DailyTimesheetSequence), ref _dailyTimesheetSequence, value);
     }
-    
+
     [RuleRequiredField]
     [ModelDefault(nameof(IModelCommonMemberViewItem.DisplayFormat), "d")]
     public DateTime Date
@@ -61,46 +58,54 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
         set => SetPropertyValue(nameof(Date), ref _date, value.Date);
     }
 
-    [ModelDefault(nameof(IModelCommonMemberViewItem.DisplayFormat), "hh\\:mm")]
+    //[ModelDefault(nameof(IModelCommonMemberViewItem.DisplayFormat), "hh\\:mm")]
     public TimeSpan TotalWork
     {
         get => _totalWork;
         protected set => SetPropertyValue(nameof(TotalWork), ref _totalWork, value);
     }
-    
+
     public bool IsLateIn
     {
         get => _isLateIn;
         protected set => SetPropertyValue(nameof(IsLateIn), ref _isLateIn, value);
     }
-    
+
     public bool IsEarlyOut
     {
         get => _isEarlyOut;
         protected set => SetPropertyValue(nameof(IsEarlyOut), ref _isEarlyOut, value);
     }
-
-    [Association("DailyTimesheet-Entries"), DevExpress.Xpo.Aggregated]
-    public XPCollection<TimesheetEntry> Entries => GetCollection<TimesheetEntry>(nameof(Entries));
     
+    [Size(SizeAttribute.Unlimited)]
+    [ModelDefault("RowCount", "3")]
+    [XafDisplayName("Notes")]
+    public string Notes
+    {
+        get => _notes;
+        set => SetPropertyValue(nameof(Notes), ref _notes, value);
+    }
+
+    [Association("DailyTimesheet-Entries")]
+    [DevExpress.Xpo.Aggregated]
+    public XPCollection<TimesheetEntry> Entries => GetCollection<TimesheetEntry>(nameof(Entries));
+
     public void Recalculate(WorkdayRule rule)
     {
-        TimeSpan total = TimeSpan.Zero;
+        var total = TimeSpan.Zero;
         DateTime? firstStart = null;
         DateTime? lastEnd = null;
 
         foreach (var e in Entries)
-        {
             if (e.EndOn.HasValue && e.EndOn.Value >= e.StartOn)
             {
-                total += (e.EndOn.Value - e.StartOn);
+                total += e.EndOn.Value - e.StartOn;
                 if (!firstStart.HasValue || e.StartOn < firstStart.Value) firstStart = e.StartOn;
                 if (!lastEnd.HasValue || e.EndOn.Value > lastEnd.Value) lastEnd = e.EndOn.Value;
             }
-        }
 
         TotalWork = total;
-        
+
         if (firstStart.HasValue)
         {
             var allowedStartMax = firstStart.Value.Date + rule.WorkdayStart + rule.ToleranceLateIn;
@@ -121,7 +126,7 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
             IsEarlyOut = false;
         }
     }
-    
+
     public override void AfterConstruction()
     {
         base.AfterConstruction();
@@ -136,15 +141,20 @@ public class DailyTimesheet(Session session) : BaseEntity(session)
         if (companyInfo == null) return;
         DailyTimesheetPrefix = companyInfo.DefaultDailyTimeSheetPrefix;
     }
-    
+
     protected override void OnSaving()
     {
         base.OnSaving();
-        if (!Session.IsNewObject(this) || !string.IsNullOrEmpty(DailyTimesheetSequence) || Session is NestedUnitOfWork) return;
+
+        if (Employee?.WorkdayRule != null) Recalculate(Employee.WorkdayRule);
+
+        if (!Session.IsNewObject(this) || !string.IsNullOrEmpty(DailyTimesheetSequence) ||
+            Session is NestedUnitOfWork) return;
         DailyTimesheetSequence =
-            SequenceFactory.GetNextSequence(Session, $"{typeof(Invoice).FullName}.{DailyTimesheetPrefix}", DailyTimesheetPrefix, 5);
+            SequenceFactory.GetNextSequence(Session, $"{typeof(Invoice).FullName}.{DailyTimesheetPrefix}",
+                DailyTimesheetPrefix, 5);
     }
-    
+
     private ApplicationUser GetCurrentUser()
     {
         return Session.GetObjectByKey<ApplicationUser>(
