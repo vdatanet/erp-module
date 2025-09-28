02969e0e99d7d465dd3e404ef3d37fb056242f02
vdatanet <joan@vdata.dev>
2025-09-28 21:47:07 +0200

    Agregar método CalculateLineTaxableAmount en SalesDocumentLineService e interfaz asociada.

--- Services/Implementations/Base/Sales/SalesDocumentLineService.cs
+++ Services/Implementations/Base/Sales/SalesDocumentLineService.cs
@@ -1,8 +1,12 @@
+using erp.Module.BusinessObjects.Base.Sales;
 using erp.Module.Services.Interfaces.Base.Sales;
 
 namespace erp.Module.Services.Implementations.Base.Sales;
 
 public class SalesDocumentLineService : ISalesDocumentLineService
 {
-    
+    public void CalculateLineTaxableAmount(SalesDocumentLine line)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Services/Interfaces/Base/Sales/ISalesDocumentLineService.cs
+++ Services/Interfaces/Base/Sales/ISalesDocumentLineService.cs
@@ -1,6 +1,8 @@
+using erp.Module.BusinessObjects.Base.Sales;
+
 namespace erp.Module.Services.Interfaces.Base.Sales;
 
 public interface ISalesDocumentLineService
 {
-    
+    void CalculateLineTaxableAmount(SalesDocumentLine line);
 }
