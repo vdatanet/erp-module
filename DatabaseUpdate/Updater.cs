e78d9dc267df70a64be6052aa0800e8c1b2e5df1
vdatanet <joan@vdata.net>
2025-11-09 10:02:19 +0100

    Ajustado formato y estilo de `Updater.cs` para seguir convenciones de codificación y mejorar legibilidad.

--- DatabaseUpdate/Updater.cs
+++ DatabaseUpdate/Updater.cs
@@ -17,11 +17,15 @@ using Microsoft.Extensions.DependencyInjection;
 namespace erp.Module.DatabaseUpdate;
 
 // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
-public class Updater : ModuleUpdater {
+public class Updater : ModuleUpdater
+{
     public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
-        base(objectSpace, currentDBVersion) {
+        base(objectSpace, currentDBVersion)
+    {
     }
-    public override void UpdateDatabaseAfterUpdateSchema() {
+
+    public override void UpdateDatabaseAfterUpdateSchema()
+    {
         base.UpdateDatabaseAfterUpdateSchema();
         //string name = "MyName";
         //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
@@ -29,15 +33,13 @@ public class Updater : ModuleUpdater {
         //    theObject = ObjectSpace.CreateObject<DomainObject1>();
         //    theObject.Name = name;
         //}
-        if (!ObjectSpace.CanInstantiate(typeof(ApplicationUser))) {
-            return;
-        }
+        if (!ObjectSpace.CanInstantiate(typeof(ApplicationUser))) return;
 
 #if !RELEASE
         //if (TenantName == null) {
-            //_ = CreateTenant("company1.com", "erp_company1");
-            //_ = CreateTenant("company2.com", "erp_company2");
-            //ObjectSpace.CommitChanges();
+        //_ = CreateTenant("company1.com", "erp_company1");
+        //_ = CreateTenant("company2.com", "erp_company2");
+        //ObjectSpace.CommitChanges();
         //}
 #endif
 
@@ -47,28 +49,33 @@ public class Updater : ModuleUpdater {
         // If a role doesn't exist in the database, create this role
         var adminRole = CreateAdminRole();
 
-        UserManager userManager = ObjectSpace.ServiceProvider.GetRequiredService<UserManager>();
+        var userManager = ObjectSpace.ServiceProvider.GetRequiredService<UserManager>();
 
-        if (TenantName != null) {
+        if (TenantName != null)
+        {
             var defaultRole = CreateDefaultRole();
 
-            string userName = $"User@{TenantName}";
+            var userName = $"User@{TenantName}";
             // If a user named 'userName' doesn't exist in the database, create this user
-            if(userManager.FindUserByName<ApplicationUser>(ObjectSpace, userName) == null) {
+            if (userManager.FindUserByName<ApplicationUser>(ObjectSpace, userName) == null)
+            {
                 // Set a password if the standard authentication type is used
-                string EmptyPassword = "";
-                _ = userManager.CreateUser<ApplicationUser>(ObjectSpace, userName, EmptyPassword, (user) => {
+                var EmptyPassword = "";
+                _ = userManager.CreateUser<ApplicationUser>(ObjectSpace, userName, EmptyPassword, (user) =>
+                {
                     // Add the Users role to the user
                     user.Roles.Add(defaultRole);
                 });
             }
         }
 
-        string adminUserName = TenantName != null ? $"Admin@{TenantName}" : "Admin";
-        if(userManager.FindUserByName<ApplicationUser>(ObjectSpace, adminUserName) == null) {
+        var adminUserName = TenantName != null ? $"Admin@{TenantName}" : "Admin";
+        if (userManager.FindUserByName<ApplicationUser>(ObjectSpace, adminUserName) == null)
+        {
             // Set a password if the standard authentication type is used
-            string EmptyPassword = "";
-            _ = userManager.CreateUser<ApplicationUser>(ObjectSpace, adminUserName, EmptyPassword, (user) => {
+            var EmptyPassword = "";
+            _ = userManager.CreateUser<ApplicationUser>(ObjectSpace, adminUserName, EmptyPassword, (user) =>
+            {
                 // Add the Administrators role to the user
                 user.Roles.Add(adminRole);
             });
@@ -76,57 +83,77 @@ public class Updater : ModuleUpdater {
 
         ObjectSpace.CommitChanges(); //This line persists created object(s).
     }
-    public override void UpdateDatabaseBeforeUpdateSchema() {
+
+    public override void UpdateDatabaseBeforeUpdateSchema()
+    {
         base.UpdateDatabaseBeforeUpdateSchema();
         //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
         //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
         //}
     }
-    Tenant CreateTenant(string tenantName, string databaseName) {
+
+    private Tenant CreateTenant(string tenantName, string databaseName)
+    {
         var tenant = ObjectSpace.FirstOrDefault<Tenant>(t => t.Name == tenantName);
-        if (tenant == null) {
+        if (tenant == null)
+        {
             tenant = ObjectSpace.CreateObject<Tenant>();
             tenant.Name = tenantName;
-            tenant.ConnectionString = $"XpoProvider=MySql;server=localhost;user=root;password=password;database={databaseName}";
+            tenant.ConnectionString =
+                $"XpoProvider=MySql;server=localhost;user=root;password=password;database={databaseName}";
         }
+
         return tenant;
     }
-    PermissionPolicyRole CreateAdminRole() {
-        PermissionPolicyRole adminRole = ObjectSpace.FirstOrDefault<PermissionPolicyRole>(r => r.Name == "Administrators");
-        if(adminRole == null) {
+
+    private PermissionPolicyRole CreateAdminRole()
+    {
+        var adminRole =
+            ObjectSpace.FirstOrDefault<PermissionPolicyRole>(r => r.Name == "Administrators");
+        if (adminRole == null)
+        {
             adminRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
             adminRole.Name = "Administrators";
             adminRole.IsAdministrative = true;
         }
+
         return adminRole;
     }
-    PermissionPolicyRole CreateDefaultRole() {
-        PermissionPolicyRole defaultRole = ObjectSpace.FirstOrDefault<PermissionPolicyRole>(role => role.Name == "Default");
-        if(defaultRole == null) {
+
+    private PermissionPolicyRole CreateDefaultRole()
+    {
+        var defaultRole =
+            ObjectSpace.FirstOrDefault<PermissionPolicyRole>(role => role.Name == "Default");
+        if (defaultRole == null)
+        {
             defaultRole = ObjectSpace.CreateObject<PermissionPolicyRole>();
             defaultRole.Name = "Default";
 
-            defaultRole.AddObjectPermissionFromLambda<ApplicationUser>(SecurityOperations.Read, cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
-            defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/MyDetails", SecurityPermissionState.Allow);
-            defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write, "ChangePasswordOnFirstLogon", cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
-            defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write, "StoredPassword", cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
-            defaultRole.AddTypePermissionsRecursively<PermissionPolicyRole>(SecurityOperations.Read, SecurityPermissionState.Deny);
-            defaultRole.AddObjectPermission<ModelDifference>(SecurityOperations.ReadWriteAccess, "UserId = ToStr(CurrentUserId())", SecurityPermissionState.Allow);
-            defaultRole.AddObjectPermission<ModelDifferenceAspect>(SecurityOperations.ReadWriteAccess, "Owner.UserId = ToStr(CurrentUserId())", SecurityPermissionState.Allow);
-            defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.Create, SecurityPermissionState.Allow);
-            defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.Create, SecurityPermissionState.Allow);
+            defaultRole.AddObjectPermissionFromLambda<ApplicationUser>(SecurityOperations.Read,
+                cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+            defaultRole.AddNavigationPermission(@"Application/NavigationItems/Items/Default/Items/MyDetails",
+                SecurityPermissionState.Allow);
+            defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write,
+                "ChangePasswordOnFirstLogon", cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(),
+                SecurityPermissionState.Allow);
+            defaultRole.AddMemberPermissionFromLambda<ApplicationUser>(SecurityOperations.Write, "StoredPassword",
+                cm => cm.Oid == (Guid)CurrentUserIdOperator.CurrentUserId(), SecurityPermissionState.Allow);
+            defaultRole.AddTypePermissionsRecursively<PermissionPolicyRole>(SecurityOperations.Read,
+                SecurityPermissionState.Deny);
+            defaultRole.AddObjectPermission<ModelDifference>(SecurityOperations.ReadWriteAccess,
+                "UserId = ToStr(CurrentUserId())", SecurityPermissionState.Allow);
+            defaultRole.AddObjectPermission<ModelDifferenceAspect>(SecurityOperations.ReadWriteAccess,
+                "Owner.UserId = ToStr(CurrentUserId())", SecurityPermissionState.Allow);
+            defaultRole.AddTypePermissionsRecursively<ModelDifference>(SecurityOperations.Create,
+                SecurityPermissionState.Allow);
+            defaultRole.AddTypePermissionsRecursively<ModelDifferenceAspect>(SecurityOperations.Create,
+                SecurityPermissionState.Allow);
         }
+
         return defaultRole;
     }
 
-    Guid? TenantId {
-        get {
-            return ObjectSpace.ServiceProvider.GetRequiredService<ITenantProvider>().TenantId;
-        }
-    }
-    string TenantName {
-        get {
-            return ObjectSpace.ServiceProvider.GetRequiredService<ITenantProvider>().TenantName;
-        }
-    }
+    private Guid? TenantId => ObjectSpace.ServiceProvider.GetRequiredService<ITenantProvider>().TenantId;
+
+    private string TenantName => ObjectSpace.ServiceProvider.GetRequiredService<ITenantProvider>().TenantName;
 }
