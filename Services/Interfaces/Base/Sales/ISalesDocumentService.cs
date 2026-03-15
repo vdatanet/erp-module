248cc59952b6ed3c8e95b909aecb5e0df275b2f3
vdatanet <joan@vdata.net>
2026-03-15 19:23:39 +0100

    Elimina servicios y métodos relacionados con `DocumentoVenta` y `LineaDocumentoVenta`.

--- Services/Interfaces/Base/Sales/ISalesDocumentService.cs
+++
@@ -1,9 +0,0 @@
-using erp.Module.BusinessObjects.Base.Ventas;
-
-namespace erp.Module.Services.Interfaces.Base.Sales;
-
-public interface IDocumentoVentaService
-{ 
-    void DeleteTaxes(DocumentoVenta salesDocument);
-    void RebuildTaxSummary(DocumentoVenta salesDocument);
-}
