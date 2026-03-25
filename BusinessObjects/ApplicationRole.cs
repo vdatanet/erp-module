a33da8fc5f5d046535d4fed793235506aa777515
vdatanet <joan@vdata.net>
2026-03-25 09:06:04 +0100

    Añade lógica para la gestión de autenticación y autorización en TPVs, incluyendo la implementación de `ApplicationRole`, `AuthMessageHandler`, `CustomAuthenticationStateProvider`, y la interfaz de selección de TPVs (`Tpvs.razor`).

---
+++ BusinessObjects/ApplicationRole.cs
@@ -0,0 +1,18 @@
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using DevExpress.Xpo;
+
+namespace erp.Module.BusinessObjects;
+
+[MapInheritance(MapInheritanceType.ParentTable)]
+public class ApplicationRole : PermissionPolicyRole
+{
+    public ApplicationRole(Session session) : base(session)
+    {
+    }
+
+    [Association("RolesAutorizados-Tpvs")]
+    [XafDisplayName("TPVs Autorizados")]
+    public XPCollection<Tpv.Tpv> TpvsAutorizados => GetCollection<Tpv.Tpv>();
+}
