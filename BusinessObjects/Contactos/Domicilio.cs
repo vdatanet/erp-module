0ae65c1ad8a4c24781004e9563c9a096c5fb9e64
vdatanet <joan@vdata.net>
2026-03-25 06:38:06 +0100

    Refactoriza `Domicilio` para heredar de `EntidadBase` en lugar de `Contacto`, añadiendo nuevos campos (e.g., Dirección, País, Teléfono) y eliminando herencia conceptual incorrecta.

--- BusinessObjects/Contactos/Domicilio.cs
+++ BusinessObjects/Contactos/Domicilio.cs
@@ -1,15 +1,25 @@
 using DevExpress.ExpressApp.DC;
 using DevExpress.Persistent.Base;
 using DevExpress.Xpo;
+using erp.Module.BusinessObjects.Auxiliares;
+using erp.Module.BusinessObjects.Base.Comun;
 
 namespace erp.Module.BusinessObjects.Contactos;
 
 [DefaultClassOptions]
 [NavigationItem("Contactos")]
 [ImageName("BO_Address")]
-public class Domicilio(Session session) : Contacto(session)
+public class Domicilio(Session session) : EntidadBase(session)
 {
     private Cliente? _clienteDomicilio;
+    private string? _direccion;
+    private Pais? _pais;
+    private Provincia? _provincia;
+    private Poblacion? _poblacion;
+    private string? _codigoPostal;
+    private string? _telefono;
+    private string? _movil;
+    private string? _correoElectronico;
 
     [XafDisplayName("Cliente")]
     [Association("Cliente-Domicilios")]
@@ -17,5 +27,65 @@ public class Domicilio(Session session) : Contacto(session)
     {
         get => _clienteDomicilio;
         set => SetPropertyValue(nameof(ClienteDomicilio), ref _clienteDomicilio, value);
+    }
+
+    [Size(255)]
+    [XafDisplayName("Dirección")]
+    public string? Direccion
+    {
+        get => _direccion;
+        set => SetPropertyValue(nameof(Direccion), ref _direccion, value);
+    }
+
+    [XafDisplayName("País")]
+    public Pais? Pais
+    {
+        get => _pais;
+        set => SetPropertyValue(nameof(Pais), ref _pais, value);
+    }
+
+    [DataSourceProperty("Pais.Provincias")]
+    [XafDisplayName("Provincia")]
+    public Provincia? Provincia
+    {
+        get => _provincia;
+        set => SetPropertyValue(nameof(Provincia), ref _provincia, value);
+    }
+
+    [DataSourceProperty("Provincia.Poblaciones")]
+    [XafDisplayName("Población")]
+    public Poblacion? Poblacion
+    {
+        get => _poblacion;
+        set => SetPropertyValue(nameof(Poblacion), ref _poblacion, value);
+    }
+
+    [Size(10)]
+    [XafDisplayName("Código Postal")]
+    public string? CodigoPostal
+    {
+        get => _codigoPostal;
+        set => SetPropertyValue(nameof(CodigoPostal), ref _codigoPostal, value);
+    }
+
+    [XafDisplayName("Teléfono")]
+    public string? Telefono
+    {
+        get => _telefono;
+        set => SetPropertyValue(nameof(Telefono), ref _telefono, value);
+    }
+
+    [XafDisplayName("Móvil")]
+    public string? Movil
+    {
+        get => _movil;
+        set => SetPropertyValue(nameof(Movil), ref _movil, value);
+    }
+
+    [XafDisplayName("Correo Electrónico")]
+    public string? CorreoElectronico
+    {
+        get => _correoElectronico;
+        set => SetPropertyValue(nameof(CorreoElectronico), ref _correoElectronico, value);
     }
 }
