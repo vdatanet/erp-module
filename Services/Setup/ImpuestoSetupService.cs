a57f491320cb61f2be1868798564a6914b14002d
vdatanet <joan@vdata.net>
2026-03-26 15:54:26 +0100

    Actualiza configuraciones de cuentas e impuestos para incluir nuevos parámetros opcionales y ajustar códigos contables.

--- Services/Setup/ImpuestoSetupService.cs
+++ Services/Setup/ImpuestoSetupService.cs
@@ -30,51 +30,51 @@ public class ImpuestoSetupService(IObjectSpace objectSpace)
     public void CreateInitialImpuestos()
     {
         // --- IVA REPERCUTIDO (Ventas S1, Régimen 01) ---
-        CreateTipoImpuesto("IVA21_REP", "IVA 21% (Repercutido)", 21, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47721");
-        CreateTipoImpuesto("IVA10_REP", "IVA 10% (Repercutido)", 10, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47710");
-        CreateTipoImpuesto("IVA4_REP", "IVA 4% (Repercutido)", 4, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47704");
-        CreateTipoImpuesto("IVA0_REP", "IVA 0% (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47700");
+        CreateTipoImpuesto("IVA21_REP", "IVA 21% (Repercutido)", 21, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4772100000");
+        CreateTipoImpuesto("IVA10_REP", "IVA 10% (Repercutido)", 10, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4771000000");
+        CreateTipoImpuesto("IVA4_REP", "IVA 4% (Repercutido)", 4, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4770400000");
+        CreateTipoImpuesto("IVA0_REP", "IVA 0% (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4770000000");
 
         // --- RECARGO DE EQUIVALENCIA REPERCUTIDO ---
-        CreateTipoImpuesto("RE52_REP", "RE 5,2% (Repercutido)", 5.2m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47752");
-        CreateTipoImpuesto("RE14_REP", "RE 1,4% (Repercutido)", 1.4m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47714");
-        CreateTipoImpuesto("RE05_REP", "RE 0,5% (Repercutido)", 0.5m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47705");
+        CreateTipoImpuesto("RE52_REP", "RE 5,2% (Repercutido)", 5.2m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4775200000");
+        CreateTipoImpuesto("RE14_REP", "RE 1,4% (Repercutido)", 1.4m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4771400000");
+        CreateTipoImpuesto("RE05_REP", "RE 0,5% (Repercutido)", 0.5m, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4770500000");
 
         // --- IVA SOPORTADO (Sin VeriFactu) ---
-        CreateTipoImpuesto("IVA21_SOP", "IVA 21% (Soportado)", 21, false, true, null, null, null, null, false, "47221");
-        CreateTipoImpuesto("IVA10_SOP", "IVA 10% (Soportado)", 10, false, true, null, null, null, null, false, "47210");
-        CreateTipoImpuesto("IVA4_SOP", "IVA 4% (Soportado)", 4, false, true, null, null, null, null, false, "47204");
-        CreateTipoImpuesto("IVA0_SOP", "IVA 0% (Soportado)", 0, false, true, null, null, null, null, false, "47200");
+        CreateTipoImpuesto("IVA21_SOP", "IVA 21% (Soportado)", 21, false, true, null, null, null, null, false, "4722100000");
+        CreateTipoImpuesto("IVA10_SOP", "IVA 10% (Soportado)", 10, false, true, null, null, null, null, false, "4721000000");
+        CreateTipoImpuesto("IVA4_SOP", "IVA 4% (Soportado)", 4, false, true, null, null, null, null, false, "4720400000");
+        CreateTipoImpuesto("IVA0_SOP", "IVA 0% (Soportado)", 0, false, true, null, null, null, null, false, "4720000000");
 
         // --- RECARGO DE EQUIVALENCIA SOPORTADO ---
-        CreateTipoImpuesto("RE52_SOP", "RE 5,2% (Soportado)", 5.2m, false, true, null, null, null, null, false, "47252");
-        CreateTipoImpuesto("RE14_SOP", "RE 1,4% (Soportado)", 1.4m, false, true, null, null, null, null, false, "47214");
-        CreateTipoImpuesto("RE05_SOP", "RE 0,5% (Soportado)", 0.5m, false, true, null, null, null, null, false, "47205");
+        CreateTipoImpuesto("RE52_SOP", "RE 5,2% (Soportado)", 5.2m, false, true, null, null, null, null, false, "4725200000");
+        CreateTipoImpuesto("RE14_SOP", "RE 1,4% (Soportado)", 1.4m, false, true, null, null, null, null, false, "4721400000");
+        CreateTipoImpuesto("RE05_SOP", "RE 0,5% (Soportado)", 0.5m, false, true, null, null, null, null, false, "4720500000");
 
         // --- IRPF (RETENCIONES PRACTICADAS - Pasivo) ---
-        CreateTipoImpuesto("IRPF15_PRACT", "IRPF Profesionales 15% (Practicada)", 15, true, true, null, null, null, null, true, "47515");
-        CreateTipoImpuesto("IRPF7_PRACT", "IRPF Nuevos Profesionales 7% (Practicada)", 7, true, true, null, null, null, null, true, "47507");
-        CreateTipoImpuesto("IRPF19_PRACT", "IRPF Alquileres 19% (Practicada)", 19, true, true, null, null, null, null, true, "47519");
+        CreateTipoImpuesto("IRPF15_PRACT", "IRPF Profesionales 15% (Practicada)", 15, true, true, null, null, null, null, true, "4751500000");
+        CreateTipoImpuesto("IRPF7_PRACT", "IRPF Nuevos Profesionales 7% (Practicada)", 7, true, true, null, null, null, null, true, "4750700000");
+        CreateTipoImpuesto("IRPF19_PRACT", "IRPF Alquileres 19% (Practicada)", 19, true, true, null, null, null, null, true, "4751900000");
 
         // --- IRPF (RETENCIONES SOPORTADAS - Activo) ---
-        CreateTipoImpuesto("IRPF15_SOP", "IRPF Profesionales 15% (Soportada)", 15, true, true, null, null, null, null, true, "47315");
-        CreateTipoImpuesto("IRPF7_SOP", "IRPF Nuevos Profesionales 7% (Soportada)", 7, true, true, null, null, null, null, true, "47307");
-        CreateTipoImpuesto("IRPF19_SOP", "IRPF Alquileres 19% (Soportada)", 19, true, true, null, null, null, null, true, "47319");
+        CreateTipoImpuesto("IRPF15_SOP", "IRPF Profesionales 15% (Soportada)", 15, true, true, null, null, null, null, true, "4731500000");
+        CreateTipoImpuesto("IRPF7_SOP", "IRPF Nuevos Profesionales 7% (Soportada)", 7, true, true, null, null, null, null, true, "4730700000");
+        CreateTipoImpuesto("IRPF19_SOP", "IRPF Alquileres 19% (Soportada)", 19, true, true, null, null, null, null, true, "4731900000");
 
         // --- EXENTOS REPERCUTIDOS (S2) ---
         // E1: Exenta por el art. 20
-        CreateTipoImpuesto("IVA_EXENTO_E1", "IVA Exento (Art. 20) (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E1, false, "47700");
+        CreateTipoImpuesto("IVA_EXENTO_E1", "IVA Exento (Art. 20) (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E1, false, "4770000000");
         // E6: Otros
-        CreateTipoImpuesto("IVA_EXENTO_E6", "IVA Exento (Otros) (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E6, false, "47700");
+        CreateTipoImpuesto("IVA_EXENTO_E6", "IVA Exento (Otros) (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E6, false, "4770000000");
 
         // --- INVERSIÓN SUJETO PASIVO REPERCUTIDO (S3) ---
-        CreateTipoImpuesto("IVA_ISP", "IVA Inversión Sujeto Pasivo (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "47700");
+        CreateTipoImpuesto("IVA_ISP", "IVA Inversión Sujeto Pasivo (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S1, null, false, "4770000000");
 
         // --- OTROS REGÍMENES REPERCUTIDOS ---
         // Operaciones Intracomunitarias (Ventas de bienes suelen ser exentas en origen E5)
-        CreateTipoImpuesto("IVA_INTRA", "IVA Intracomunitario (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E5, false, "47700");
+        CreateTipoImpuesto("IVA_INTRA", "IVA Intracomunitario (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E5, false, "4770000000");
         // Exportaciones (E2)
-        CreateTipoImpuesto("IVA_EXPORT", "Exportaciones (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E2, false, "47700");
+        CreateTipoImpuesto("IVA_EXPORT", "Exportaciones (Repercutido)", 0, true, false, Impuesto.IVA, (ClaveRegimen)1, CalificacionOperacion.S2, CausaExencion.E2, false, "4770000000");
     }
 
     private void CreateTipoImpuesto(string codigo, string nombre, decimal tipo, bool enVentas, bool enCompras,
